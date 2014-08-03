d0eb3dd663c56c07fbe162a6fffc1680f048382c
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-08-03 11:28:28 -0400

    Wrap whole program in try-catch

--- hammer_backstock/Program.cs
+++ hammer_backstock/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System;
 
 namespace hammer_backstock
 {
@@ -9,41 +10,49 @@ namespace hammer_backstock
     {
         static void Main(string[] args)
         {
-            // Ensure I'm the only hammer
-            if (Process.GetProcessesByName("hammer").Count() > 0)
+            try
             {
-                MessageBox.Show("Backstock Hammer can only support one instance at a time");
-                return;
+                // Ensure I'm the only hammer
+                if (Process.GetProcessesByName("hammer").Count() > 0)
+                {
+                    MessageBox.Show("Backstock Hammer can only support one instance at a time");
+                    return;
+                }
+
+                // Move configurations into place
+                if (File.Exists("CmdSeq.wc")) File.Copy("CmdSeq.wc", "CmdSeq_original.wc", true);
+                if (File.Exists("CmdSeq_backstock.wc")) File.Copy("CmdSeq_backstock.wc", "CmdSeq.wc", true);
+
+                if (File.Exists("GameConfig.txt")) File.Copy("GameConfig.txt", "GameConfig_original.txt", true);
+                if (File.Exists("GameConfig_backstock.txt")) File.Copy("GameConfig_backstock.txt", "GameConfig.txt", true);
+
+                //Start hammer
+                var ccTagIntermediateCompilerProcess = new Process();
+                ccTagIntermediateCompilerProcess.StartInfo.FileName = "hammer.exe";
+                ccTagIntermediateCompilerProcess.StartInfo.Arguments = "-nop4";
+                ccTagIntermediateCompilerProcess.Start();
+                ccTagIntermediateCompilerProcess.WaitForExit();
+
+                // Restore original configurations
+
+                if (File.Exists("CmdSeq.wc")) File.Copy("CmdSeq.wc", "CmdSeq_backstock.wc", true);
+                if (File.Exists("CmdSeq_original.wc"))
+                {
+                    File.Copy("CmdSeq_original.wc", "CmdSeq.wc", true);
+                    File.Delete("CmdSeq_original.wc");
+                }
+
+                if (File.Exists("GameConfig.txt")) File.Copy("GameConfig.txt", "GameConfig_backstock.txt", true);
+                if (File.Exists("GameConfig_original.txt"))
+                {
+                    File.Copy("GameConfig_original.txt", "GameConfig.txt", true);
+                    File.Delete("GameConfig_original.txt");
+                }
             }
-
-            // Move configurations into place
-            if (File.Exists("CmdSeq.wc")) File.Copy("CmdSeq.wc", "CmdSeq_original.wc", true);
-            if (File.Exists("CmdSeq_backstock.wc")) File.Copy("CmdSeq_backstock.wc", "CmdSeq.wc", true);
-
-            if (File.Exists("GameConfig.txt")) File.Copy("GameConfig.txt", "GameConfig_original.txt", true);
-            if (File.Exists("GameConfig_backstock.txt")) File.Copy("GameConfig_backstock.txt", "GameConfig.txt", true);
-
-            //Start hammer
-            var ccTagIntermediateCompilerProcess = new Process();
-            ccTagIntermediateCompilerProcess.StartInfo.FileName = "hammer.exe";
-            ccTagIntermediateCompilerProcess.StartInfo.Arguments = "-nop4";
-            ccTagIntermediateCompilerProcess.Start();
-            ccTagIntermediateCompilerProcess.WaitForExit();
-
-            // Restore original configurations
-
-            if (File.Exists("CmdSeq.wc")) File.Copy("CmdSeq.wc", "CmdSeq_backstock.wc", true);
-            if (File.Exists("CmdSeq_original.wc"))
+            catch (Exception ex)
             {
-                File.Copy("CmdSeq_original.wc", "CmdSeq.wc", true);
-                File.Delete("CmdSeq_original.wc");
-            }
-
-            if (File.Exists("GameConfig.txt")) File.Copy("GameConfig.txt", "GameConfig_backstock.txt", true);
-            if (File.Exists("GameConfig_original.txt"))
-            {
-                File.Copy("GameConfig_original.txt", "GameConfig.txt", true);
-                File.Delete("GameConfig_original.txt");
+                File.AppendAllText("hammer_backstock_exceptions.txt", ex.ToString() + Environment.NewLine + Environment.NewLine + 
+                    "================================================================================================================================");
             }
         }
     }
